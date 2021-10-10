96e327c69bc23fc27fb0ceaa1f38f4a4a65d4bc9
Alexey Voznyuk <me@swizard.info>
2021-10-10 16:57:39 +0300

    phrase generator alpha

--- src/vaccine_reminder.rs
+++ src/vaccine_reminder.rs
@@ -6,6 +6,8 @@ use structopt::{
     StructOpt,
 };
 
+use rand::Rng;
+
 use telegram_bot::{
     types::{
         UserId,
@@ -64,14 +66,11 @@ impl VaccineReminder {
                         kind: MessageKind::Text { data, .. },
                         ..
                     } if user_id == &self.user_id && chat_id == &self.group_id && is_question(data) => {
-
-                        // if let UpdateKind::Message(message) = &update.kind {
-                        //     if let MessageKind::Text { data, .. } = &message.kind {
-
                         println!("<{}>: {}", &message.from.first_name, data);
                         println!("{:?}", update);
 
-                        let _message_or_channel_post = api.send(message.text_reply("Эй, как насчёт укола в жопу?")).await
+                        let reply_phrase = build_phrase();
+                        let _message_or_channel_post = api.send(message.text_reply(reply_phrase)).await
                             .map_err(Error::TelegramApiSend)?;
                     },
                     other_message =>
@@ -95,4 +94,131 @@ fn is_question(message: &str) -> bool {
     }
 
     false
+}
+
+fn build_phrase() -> String {
+    let mut rng = rand::thread_rng();
+
+    let mut phrase = String::new();
+
+    if let Some(address) = phrase_address(&mut rng) {
+        phrase.push_str(address);
+        phrase.push(' ');
+    }
+    if let Some(name) = phrase_name(&mut rng) {
+        phrase.push_str(name);
+        phrase.push_str(", ");
+    }
+    phrase.push_str(phrase_start(&mut rng));
+    phrase.push(' ');
+    phrase.push_str(phrase_action(&mut rng));
+    phrase.push(' ');
+    phrase.push_str(phrase_vaccination(&mut rng));
+    if let Some(covid) = phrase_covid(&mut rng) {
+        phrase.push(' ');
+        phrase.push_str(covid);
+    }
+    if let Some(terminate) = phrase_terminate(&mut rng) {
+        phrase.push_str(terminate);
+    }
+    phrase.push('?');
+
+    phrase
+}
+
+fn phrase_address<R>(rng: &mut R) -> Option<&'static str> where R: Rng {
+    let variants = &[
+        "Уважаемый",
+        "Многоуважаемый",
+        "Эй,",
+        "Слушай,",
+        "Послушай,",
+        "Извини,",
+        "Прошу прощения за беспокойство,",
+        "Дружище",
+        "Коллега",
+    ];
+
+    random_variant_opt(rng, variants)
+}
+
+fn phrase_name<R>(rng: &mut R) -> Option<&'static str> where R: Rng {
+    let variants = &[
+        "Ахмед",
+        "Али Баба",
+        "Парвиз",
+    ];
+
+    random_variant_opt(rng, variants)
+}
+
+fn phrase_start<R>(rng: &mut R) -> &'static str where R: Rng {
+    let variants = &[
+        "а ты уже",
+        "ты ещё не",
+        "скажи, ты",
+        "подскажи, ты",
+        "неужели ты",
+    ];
+
+    random_variant(rng, variants)
+}
+
+fn phrase_action<R>(rng: &mut R) -> &'static str where R: Rng {
+    let variants = &[
+        "сделал",
+        "выполнил",
+        "совершил",
+        "произвёл",
+    ];
+
+    random_variant(rng, variants)
+}
+
+fn phrase_vaccination<R>(rng: &mut R) -> &'static str where R: Rng {
+    let variants = &[
+        "прививку",
+        "вакцинацию",
+        "укол",
+    ];
+
+    random_variant(rng, variants)
+}
+
+fn phrase_covid<R>(rng: &mut R) -> Option<&'static str> where R: Rng {
+    let variants = &[
+        "от коронавируса",
+        "от ковида",
+        "от covid-19",
+        "от понятно какой болезни",
+    ];
+
+    random_variant_opt(rng, variants)
+}
+
+fn phrase_terminate<R>(rng: &mut R) -> Option<&'static str> where R: Rng {
+    let variants = &[
+        ", наконец",
+        ", в конце концов",
+        ", наконец-то",
+        ", в конечном итоге",
+    ];
+
+    random_variant_opt(rng, variants)
+}
+
+fn random_variant_opt<R>(rng: &mut R, variants: &[&'static str]) -> Option<&'static str> where R: Rng {
+    let variants_count = variants.len();
+    let index = rng.gen_range(0 ..= variants_count);
+    if index == 0 {
+        None
+    } else {
+        Some(variants[index - 1])
+    }
+}
+
+fn random_variant<R>(rng: &mut R, variants: &[&'static str]) -> &'static str where R: Rng {
+    let variants_count = variants.len();
+    let index = rng.gen_range(0 .. variants_count);
+    variants[index]
 }
